4872e7e68dfcac80bace4f10345d603aa5d6bb6d
Eugen Karaman <karamanjekakol@gmail.com>
2024-02-08 18:38:46 +0200

    Add project files.

---
+++ Classes/Digit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sixth_Practice.Digit
+{
+    public class Text
+    {
+        public string str = "abcd1234";
+        public string onlyDigit;
+
+        public string Get()
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLetter(str[i])) { onlyDigit += str[i]; }
+            }
+            return onlyDigit;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using Digit = Sixth_Practice.Digit;
+using Letter = Sixth_Practice.Letter;
+using System.Diagnostics;
+
+namespace Sixth_Practice
+{
+    class Program
+    {
+        static void Main()
+        {
+            Digit.Text digit = new Digit.Text();
+            Letter.Text letter = new Letter.Text();
+            Console.WriteLine($"Digits: {digit.Get()}\nLetter: {letter.Get()}");
+        }
+    }
+}
+
